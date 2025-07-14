2ea9266bbbbf3bddb3862855627f137354189528
KirlosNabil <kirlosnabil956@gmail.com>
2025-07-14 03:17:22 +0300

    Add logistic regression model training and resume dataset

--- Resume_Analyzer_AI_Model/main.py
+++ Resume_Analyzer_AI_Model/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from utils import clean_resume
 from schemas import ResumeAIRequest, ResumeAIResponse, PredictedJob
+import pandas as pd
 
 app = FastAPI()
 
---
+++ Resume_Analyzer_AI_Model/train_model.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv("data/resumes_dataset.csv")
+df = df.dropna(subset=["resume_text", "job_title"])
+
+vectorizer = TfidfVectorizer(max_features=3000, stop_words='english')
+X = vectorizer.fit_transform(df["resume_text"])
+
+label_encoder = LabelEncoder()
+y = label_encoder.fit_transform(df["job_title"])
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
